fa668d64464e74d5053e3a2451e267cb0fa7cf70
dickydev <dickyfernando062@gmail.com>
2024-12-18 10:05:07 +0700

    first commit

---
+++ e-hibah/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
